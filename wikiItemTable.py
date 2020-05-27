6b3e6e64ec7e61e2d45f957041c6738ae230fb5a
plsck <plscks@icloud.com>
2020-05-27 06:53:09 -0500

    Updates items with DONE that have been added to wiki

--- wikiItemTable.py
+++ wikiItemTable.py
@@ -40,7 +40,7 @@ def wikiOutParse(masterItemRates):
                 outsideOutput.append('|-\n')
                 outsideOutput.append(f'| [[{location[0]}]] || {location[1][1]} || {round(location[1][2], 2)} || {round(location[1][0], 2)}\n')
             outsideOutput.append('|}\n')
-        with open('wikiItemTables.txt', 'a') as file:
+        with open('wikiItemTables2.txt', 'a') as file:
             file.write(f'~~~ {itemName}~~~\n')
             file.write('\n')
             for line in insideOutput:
